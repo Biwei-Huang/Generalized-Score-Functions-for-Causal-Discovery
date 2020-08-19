5739b84390076b1e0f1f24fad93277a8fec0128b
Large-scale Causal Discovery <bwei.huang@gmail.com>
2020-08-18 23:35:58 -0400

    Update example1.m

--- GES_with_generalized_score/example1.m
+++ GES_with_generalized_score/example1.m
@@ -12,14 +12,16 @@ X=X*diag(1./std(X));
 maxP = 5; % maximum number of parents when searching the graph
 parameters.kfold = 10; % 10 fold cross validation
 parameters.lambda = 0.01;
-Record = GES(X,1,maxP,parameters);
+multi_sign = 0;
+Record = GES(X,1,multi_sign,maxP,parameters);
 
-%function [Record] = GES(X,score_type,maxP,parameters)
+%function [Record] = GES(X,score_type,multi_sign,maxP,parameters)
 % INPUT:
 % X: Data with T*D dimensions
 % score_type: the score function you want to use
 %               score_type = 1: cross-validated likelihood
 %               score_type = 2: marginal likelihood
+% multi_sign: 1: if are multi-dimensional variables; 0: otherwise
 % maxP: allowed maximum number of parents when searching the graph
 % parameters: when using CV likelihood, 
 %               parameters.kfold: k-fold cross validation
@@ -27,6 +29,9 @@ Record = GES(X,1,maxP,parameters);
 
 % OUTPUT:
 % Record.G: learned causal graph
+%        G(i,j) = 1: i->j (the edge is from i to j)
+%        G(i,j) = -1: i-j (the direction between i and j is not determined)
+%        G(i,j) = 0:  i j (no causal edge between i and j)
 % Record.update1: each update (Insert operator) in the forward step
 % Record.update2: each update (Delete operator) in the backward step
 % Record.G_step1: learned graph at each step in the forward step
